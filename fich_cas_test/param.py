af4d7fb37be6b3a4d4174b3100f695d80e8792f7
Camille Pouzac <camille.pouzac@enseirb-matmeca.fr>
2025-11-17 11:17:45 +0100

    fichier param

--- fich_cas_test/param.py
+++ fich_cas_test/param.py
@@ -15,7 +15,7 @@ for it in range(Nt):
 # PARAM: liste des réactions: codage pour dire e^-+Ar->B+C et B+C->Ar+K+L et e^-+B->C
 list_reac={0 : "e^- Ar B C", 1 : "B C Ar K L", 2 : "e^- B C"}
 # PARAM: liste des types de réactions: "binaire" indique qu'il y a 2 réactifs, "unaire" qu'il y en a qu'un seul, "ternaire" qu'il y en a 3
-list_type={0:"binaire", 1:"binaire", 2:"binaire", 3:"ternaire"}
+list_type={0:"binaire", 1:"binaire", 2:"binaire"}
 # les constantes des réactions
 sig_r_0 = 1.0;
 sig_r_1 = 2.0;
