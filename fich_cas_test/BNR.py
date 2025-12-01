03f08f95169622da8e9ce7548cd02f53953ef6fc
Poette <gael.poette@gmail.com>
2025-12-01 11:13:45 +0100

    bnr ok

--- fich_cas_test/BNR.py
+++ fich_cas_test/BNR.py
@@ -5,7 +5,7 @@ list_cas_bnr=["premier_test"]
 
 for cas in list_cas_bnr: 
     os.chdir(cas)
-    os.system("cp param.py ../..")
+    os.system("cp param.json ../..")
     os.system("python3 ../../ode_mc.py")
     os.system("diff rez.txt rez_ref.txt > listing")
     print("si la commande renvoie 0, pas de différences")
